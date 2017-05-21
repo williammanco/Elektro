20ad87c4a487d578ee4f980b055cbf7f5b180cbd
williammanco <wmanco88@gmail.com>
2017-05-21 21:35:16 +0200

    fix

--- src/client/canvas.js
+++ src/client/canvas.js
@@ -100,8 +100,8 @@ export default class Canvas {
     if(type == 'mousemove'){
       nx = x / window.innerWidth * 2 - 1
       ny = -y / window.innerHeight * 2 + 1
-      ry = -Math.mapLinear(nx, -1, 1, cameraPanRange * -0.5, cameraPanRange * 0.5)
-      rx = Math.mapLinear(ny, -1, 1, cameraYawRange * -0.5, cameraYawRange * 0.5)
+      ry = -THREE.Math.mapLinear(nx, -1, 1, cameraPanRange * -0.5, cameraPanRange * 0.5)
+      rx = THREE.Math.mapLinear(ny, -1, 1, cameraYawRange * -0.5, cameraYawRange * 0.5)
     }else{
       rx = x * 0.01
       ry = y * 0.03
